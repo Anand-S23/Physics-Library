19012f22ef8ce5ac777fb823ebd5f14710e5efd9
Anand-S23 <anandsroopsingh23@gmail.com>
2019-10-27 20:43:20 -0400

    delta x with X=Xo+Vo*t+.5a*t^2

--- kinematics.py
+++ kinematics.py
@@ -2,6 +2,8 @@ class Kinematics:
     def __init__(self, **kwargs): 
         givens = {
             'delta_x': None,
+            'initial_x': None,
+            'final_x': None,
             'initial_vel': None, 
             'final_vel': None,
             'acc': None,
@@ -15,29 +17,30 @@ class Kinematics:
         # delta x = (delta v / 2) * t
         if None not in (self.initial_vel, self.final_vel, self.time):
             self.delta_x = ((self.initial_vel + self.final_vel) / 2) * self.time 
-            return self.delta_x
+        elif None not in (self.initial_vel, self.acc, self.time):
+            self.delta_x = self.initial_vel * self.time + .5 * self.acc * self.time**2 
         else:
             return "NOT ENOUGH GIVENS"
+        return self.delta_x
         
-    
     def final_velocity(self):
         # V = Vo + at
         if None not in (self.initial_vel, self.acc, self.time):
             self.final_vel = self.initial_vel + self.acc * self.time
-            return self.final_vel
+        # V = 2 delta x / t - Vo
         elif None not in (self.initial_vel, self.final_vel, self.time):
             self.final_vel = 2*self.delta_x / self.time - self.initial_vel
-            return self.delta_x
         else:
             return "NOT ENOUGH GIVENS"
+        return self.final_vel
     
     def ave_acceleration(self):
         # a = (V - Vo) / t
         if None not in (self.initial_vel, self.final_vel, self.time):
             self.acc = (self.final_vel - self.initial_vel) / self.time
-            return self.acc
         else: 
             return "NOT ENOUGH GIVENS"
+        return self.acc
 
 # Testing purposes
 if __name__ == '__main__':
