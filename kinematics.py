85dde7ad34d6386f15d15358f05505d28a60c346
Anand-S23 <anandsroopsingh23@gmail.com>
2019-10-27 20:22:21 -0400

    delta_x added

--- kinematics.py
+++ kinematics.py
@@ -1,32 +1,45 @@
 class Kinematics:
     def __init__(self, **kwargs): 
         givens = {
-            'init_vel': None, 
+            'delta_x': None,
+            'initial_vel': None, 
             'final_vel': None,
             'acc': None,
-            'delta_time': None
+            'time': None,
         }
 
         for (field, val) in givens.items():
             setattr(self, field, kwargs.get(field, val))
     
+    def delta_x(self):
+        # delta x = (delta v / 2) * t
+        if None not in (self.initial_vel, self.final_vel, self.time):
+            self.delta_x = ((self.initial_vel + self.final_vel) / 2) * self.time 
+            return self.delta_x
+        else:
+            return "NOT ENOUGH GIVENS"
+        
+    
     def final_velocity(self):
         # V = Vo + at
-        if None not in (self.init_vel, self.acc, self.delta_time):
-            self.final_vel = self.init_vel + self.acc * self.delta_time
+        if None not in (self.initial_vel, self.acc, self.time):
+            self.final_vel = self.initial_vel + self.acc * self.time
             return self.final_vel
+        elif None not in (self.initial_vel, self.final_vel, self.time):
+            self.final_vel = 2*self.delta_x / self.time - self.initial_vel
+            return self.delta_x
         else:
             return "NOT ENOUGH GIVENS"
     
-    def average_acc(self):
+    def ave_acceleration(self):
         # a = (V - Vo) / t
-        if None not in (self.init_vel, self.final_vel, self.delta_time):
-            self.acc = (self.final_vel - self.init_vel) / self.delta_time
+        if None not in (self.initial_vel, self.final_vel, self.time):
+            self.acc = (self.final_vel - self.initial_vel) / self.time
             return self.acc
         else: 
-            return 
+            return "NOT ENOUGH GIVENS"
 
 # Testing purposes
 if __name__ == '__main__':
-    x = Kinematics(init_vel=5, acc=-9.8, delta_time=10)
+    x = Kinematics(initial_vel=5, acc=-9.8, time=10)
     print(x.final_velocity())
