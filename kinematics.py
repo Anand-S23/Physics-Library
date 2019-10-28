0bd63d914fa71485f13cdda1894b9106ecd8f8a9
Anand-S23 <anandsroopsingh23@gmail.com>
2019-10-27 20:54:43 -0400

    fixing function name error

--- kinematics.py
+++ kinematics.py
@@ -1,7 +1,7 @@
 class Kinematics:
     def __init__(self, **kwargs): 
         givens = {
-            'delta_x': None,
+            'dx': None,
             'initial_x': None,
             'final_x': None,
             'initial_vel': None, 
@@ -16,12 +16,12 @@ class Kinematics:
     def delta_x(self):
         # delta x = (delta v / 2) * t
         if None not in (self.initial_vel, self.final_vel, self.time):
-            self.delta_x = ((self.initial_vel + self.final_vel) / 2) * self.time 
+            self.dx = ((self.initial_vel + self.final_vel) / 2) * self.time 
         elif None not in (self.initial_vel, self.acc, self.time):
-            self.delta_x = self.initial_vel * self.time + .5 * self.acc * self.time**2 
+            self.dx = self.initial_vel * self.time + .5 * self.acc * self.time**2 
         else:
             return "NOT ENOUGH GIVENS"
-        return self.delta_x
+        return self.dx
         
     def final_velocity(self):
         # V = Vo + at
@@ -29,7 +29,7 @@ class Kinematics:
             self.final_vel = self.initial_vel + self.acc * self.time
         # V = 2 delta x / t - Vo
         elif None not in (self.initial_vel, self.final_vel, self.time):
-            self.final_vel = 2*self.delta_x / self.time - self.initial_vel
+            self.final_vel = 2*self.dx / self.time - self.initial_vel
         else:
             return "NOT ENOUGH GIVENS"
         return self.final_vel
@@ -45,4 +45,6 @@ class Kinematics:
 # Testing purposes
 if __name__ == '__main__':
     x = Kinematics(initial_vel=5, acc=-9.8, time=10)
+    x2 = Kinematics(initial_vel=5, final_vel=10, time=2)
     print(x.final_velocity())
+    print(x2.delta_x())
